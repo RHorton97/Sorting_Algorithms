55b0bafdabfeb3112d67a5c849c0dabc37b8a454
Richard Horton <richardhorton97@outlook.com>
2019-01-08 13:28:26 +0000

    Created InsertionSort.py

---
+++ InsertionSort.py
@@ -0,0 +1,13 @@
+#Insertion Sort
+
+def insertionSort(unsorted):
+    '''Insertion Sort
+       Takes an list of comparable values and sorts them using
+       insertion sort before returning them in a list'''
+    pass()
+
+if __name__ == "__main__":
+    a = [4, 1, 8, 10, 3, 6]
+    print("Unsorted:", a)
+    a = insertionSort(a)
+    print("Sorted:", a)
