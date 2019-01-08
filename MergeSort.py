a079609cffc3b4af781a1894adea6902e0a43c55
Richard Horton <richardhorton97@outlook.com>
2019-01-08 17:38:30 +0000

    Revert "Revert "Implemented Merge Sort""
    This reverts commit 37e6c0343f03b8322666d090c2d0fa18bc6d1f52.

--- MergeSort.py
+++ MergeSort.py
@@ -10,13 +10,45 @@ def mergeSort(aList):
     if len(aList) <= 1:
         return aList
 
-    midPoint
+    #Splits the list in two
+    midPoint = len(aList)//2
+    list1 = aList[:midPoint]
+    list2 = aList[midPoint:]
+
+    #Sorts each half
+    list1 = mergeSort(list1)
+    list2 = mergeSort(list2)
+
+    return merge(list1, list2) #Returns the sorted list
 
 def merge(list1, list2):
     '''Merge function for MergeSort to call
        Merges two lists together into a single sorted list before
        returning that list'''
-    pass()
+    output = list() #Creates list to store sorted values in
+    i = 0 #Indexes to cycle through the lists
+    j = 0
+
+    #Loops through values until one list is completed
+    while i < len(list1) and j < len(list2):
+        if list1[i] < list2[j]:
+            output.append(list1[i])
+            i = i + 1
+        else:
+            output.append(list2[j])
+            j = j + 1
+
+    #Checks for leftover values in list1
+    while i < len(list1): 
+        output.append(list1[i]) 
+        i = i + 1
+
+    #Checks for leftover values in list2
+    while j < len(list2): 
+        output.append(list2[j]) 
+        j = j + 1
+
+    return output
 
 if __name__ == "__main__":
     a = [4, 1, 8, 10, 3, 6]
