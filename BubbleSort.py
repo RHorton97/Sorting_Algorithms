fbb05cc9a2f231e2910244967143597ad4f6261b
Richard Horton <richardhorton97@outlook.com>
2019-01-08 13:42:48 +0000

    Created BubbleSort.py

---
+++ BubbleSort.py
@@ -0,0 +1,16 @@
+#Bubble Sort
+#Best Case: O(n)
+#Average Case: O(n^2)
+#Worst Case: O(n^2)
+
+def bubbleSort(aList):
+    '''Bubble sort
+       Takes an list of comparable values and sorts them using
+       bubble sort before returning them in a list'''
+    pass()
+
+if __name__ == "__main__":
+    a = [4, 1, 8, 10, 3, 6]
+    print("Unsorted:", a)
+    a = selectionSort(a)
+    print("Sorted:", a)
