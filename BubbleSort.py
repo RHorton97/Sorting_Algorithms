912a0cc38d28fcc977391dfd3b22de63f667ac0b
Richard Horton <richardhorton97@outlook.com>
2019-01-08 13:56:05 +0000

    Implemented Bubble Sort

--- BubbleSort.py
+++ BubbleSort.py
@@ -7,10 +7,23 @@ def bubbleSort(aList):
     '''Bubble sort
        Takes an list of comparable values and sorts them using
        bubble sort before returning them in a list'''
-    pass()
+    swap = True #Variable to track if a swap has happened
+    
+    while swap:
+        swap = False #Breaks loop if no swap happens on any given pass
+        for i in range(0, len(aList)-1): #Loops through list
+            j = i + 1 #Sets second value of pair
+            if aList[i] > aList[j]: #If values aren't ordered, swap them
+                swap = True #Marks that a swap has happened on this pass
+                iVal = aList[i]
+                jVal = aList[j]
+                aList[i] = jVal
+                aList[j] = iVal
+
+    return aList
 
 if __name__ == "__main__":
     a = [4, 1, 8, 10, 3, 6]
     print("Unsorted:", a)
-    a = selectionSort(a)
+    a = bubbleSort(a)
     print("Sorted:", a)
