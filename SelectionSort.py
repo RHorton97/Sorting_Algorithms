2a3f9323a3fa4367f49a1d64b74050eed2e7ed70
Richard Horton <richardhorton97@outlook.com>
2019-01-08 12:57:52 +0000

    Implemented Selection Sort
    Simple selection sort implementation for Python lists

---
+++ SelectionSort.py
@@ -0,0 +1,28 @@
+#Selection Sort
+
+def selectionSort(unsorted):
+    '''Selection sort
+       Takes an list of comparable values and returns them in
+       in a sorted list'''
+    
+    sortedList = list()
+    while unsorted != []: #Cycles through the unsorted list
+        small = None
+        for item in unsorted: #Finds the smallest item in the unsorted list
+            if small == None:
+                small = item
+            else:
+                if item < small:
+                    small = item
+                    
+        sortedList.append(small) #Moves smallest item to sorted list
+        index = unsorted.index(small) #Uses index to prevent duplicate deletion
+        unsorted.pop(index) #Removes smallest item from unsorted list
+
+    return sortedList
+
+if __name__ == "__main__":
+    a = [4, 1, 8, 10, 3, 6]
+    print("Unsorted:", a)
+    a = selectionSort(a)
+    print("Sorted:", a)
