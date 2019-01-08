bc0eaaf449ccf073b7a18681c6f68466f8327f17
Richard Horton <richardhorton97@outlook.com>
2019-01-08 16:11:38 +0000

    Created MergeSort.py

---
+++ MergeSort.py
@@ -0,0 +1,25 @@
+#Merge Sort
+#Best Case: O(n log(n))
+#Average Case: O(n log(n))
+#Worst Case: O(n log(n))
+
+def mergeSort(aList):
+    '''Merge sort
+       Takes an list of comparable values and sorts them using
+       merge sort before returning them in a list'''
+    if len(aList) <= 1:
+        return aList
+
+    midPoint
+
+def merge(list1, list2):
+    '''Merge function for MergeSort to call
+       Merges two lists together into a single sorted list before
+       returning that list'''
+    pass()
+
+if __name__ == "__main__":
+    a = [4, 1, 8, 10, 3, 6]
+    print("Unsorted:", a)
+    a = mergeSort(a)
+    print("Sorted:", a)
